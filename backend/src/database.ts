5ab4a4ac4982495a33c8f256562aba8498efbe40
nikolastsiamp-glitch <nikolastsiamp@gmail.com>
2026-04-21 17:06:14 +0300

    added callendar checkbox avatar

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -4,7 +4,7 @@ import {createPool, Pool, PoolOptions} from 'mysql2/promise';
 const mysqlOptions: PoolOptions = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASS,
     database: process.env.DB_SCHEMA
 }
 
